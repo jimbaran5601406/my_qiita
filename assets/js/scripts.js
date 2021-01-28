bc75d68d42c605a55f4e7dab1d4a0357bfda1fb1
KeiFunatsuya <jimbaran5601406@gmail.com>
2021-01-28 15:03:56 +0900

    modified index.js to get rid of some warnings

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,88 +7,98 @@
 (function ($) {
 	"use strict"; // Start of use strict
 
-	$(function() {
-		const NAVBAR = document.querySelector(".navbar-nav");
-		const NAVBAR_BRAND_HEIGHT = document.querySelector(".navbar-brand").clientHeight;
-		const NAVBAR_HEIGHT = NAVBAR.clientHeight;
-		const WINDOW_HEIGHT = window.innerHeight;
-		const IS_OVERFLOW = WINDOW_HEIGHT < NAVBAR_BRAND_HEIGHT + NAVBAR_HEIGHT;
+	// aosライブラリ初期化
+	AOS.init();
 
-		$(".navbar-brand").addClass("show");
-		$(".navbar-nav").addClass("show");
-		if(IS_OVERFLOW) {
-			NAVBAR.classList.add("overflow");
+	{ // ローディングアニメーション非表示
+		setTimeout(function () {
+			$("#loading").addClass("loaded");
+			$("#content").addClass("loaded");
+		}, 1200);
+	}
+
+
+	if(document.querySelector(".post")) { // 投稿ページ限定処理
+		{ // ナビゲーションが画面の高さを超過した時の処理
+			const NAVBAR = document.querySelector(".navbar-nav");
+			const NAVBAR_BRAND_HEIGHT = document.querySelector(".navbar-brand").clientHeight;
+			const NAVBAR_HEIGHT = NAVBAR.clientHeight;
+			const WINDOW_HEIGHT = window.innerHeight;
+			const IS_OVERFLOW = WINDOW_HEIGHT < NAVBAR_BRAND_HEIGHT + NAVBAR_HEIGHT;
+
+			$(".navbar-brand").addClass("show");
+			$(".navbar-nav").addClass("show");
+			if(IS_OVERFLOW) {
+				NAVBAR.classList.add("overflow");
+			}
 		}
-	})
-	$(function () {
-		// コメント欄の送信ボタン取得
-		const COMMENT_SUBMIT_BTN = document.querySelector("#comment_submit");
-		if (COMMENT_SUBMIT_BTN) {
-			// bootstrapのクラス名付与
-			COMMENT_SUBMIT_BTN.setAttribute(
-				"class",
-				"btn btn-outline-primary btn-block"
+
+		{ // コメント送信ボタンにbootstrapクラス名付与
+			const COMMENT_SUBMIT_BTN = document.querySelector("#comment_submit");
+			if (COMMENT_SUBMIT_BTN) {
+				// bootstrapのクラス名付与
+				COMMENT_SUBMIT_BTN.setAttribute(
+					"class",
+					"btn btn-outline-primary btn-block"
+				);
+			}
+		}
+
+		{ // 各見出しにid付与
+			// 投稿記事内のh1～h6までの見出し取得
+			const HEADINGS = document.querySelectorAll(
+				".post h2, .post h3, .post h4, .post h5, .post h6"
 			);
+			// 各見出しにhd_indexというID付与
+			HEADINGS.forEach(function (heading, i) {
+				heading.setAttribute("id", `hd_${i}`);
+			});
 		}
-	});
 
-	$(function () {
-		// 投稿記事内のh1～h6までの見出し取得
-		const HEADINGS = document.querySelectorAll(
-			".post h2, .post h3, .post h4, .post h5, .post h6"
-		);
-		// 各見出しにhd_indexというID付与
-		HEADINGS.forEach(function (heading, i) {
-			heading.setAttribute("id", `hd_${i}`);
-		});
-	});
+		{ // 拍手アイコンとコメントアイコンの表示処理
+			window.addEventListener("scroll", displayClapBtn);
 
-	$(function () {
-		window.addEventListener("scroll", displayClapBtn);
+			function displayClapBtn() {
+				const WINDOW_WIDTH = window.innerWidth;
+				if (WINDOW_WIDTH >= 992) {
+					const WINDOW_HEIGHT = window.innerHeight / 3;
+					const VISIBLE_TOP_BORDER =
+						document.querySelector(".post").offsetTop - WINDOW_HEIGHT;
+					const VISIBLE_BOTTOM_BORDER =
+						document.querySelector(".post-pagination").offsetTop - WINDOW_HEIGHT;
+					const CLAP_BTN = document.querySelector(".post .wpulike");
+					const COMMENT_BTN = document.querySelector(".post .post-comment-btn");
 
-		function displayClapBtn() {
-			const WINDOW_WIDTH = window.innerWidth;
-			if (WINDOW_WIDTH >= 992) {
-				const WINDOW_HEIGHT = window.innerHeight / 3;
-				const VISIBLE_TOP_BORDER =
-					document.querySelector(".post").offsetTop - WINDOW_HEIGHT;
-				const VISIBLE_BOTTOM_BORDER =
-					document.querySelector(".post-pagination").offsetTop - WINDOW_HEIGHT;
-				const CLAP_BTN = document.querySelector(".post .wpulike");
-				const COMMENT_BTN = document.querySelector(".post .post-comment-btn");
-
-				if (
-					window.scrollY >= VISIBLE_TOP_BORDER &&
-					window.scrollY <= VISIBLE_BOTTOM_BORDER
-				) {
-					CLAP_BTN.classList.add("show");
-					COMMENT_BTN.classList.add("show");
-				} else {
-					CLAP_BTN.classList.remove("show");
-					COMMENT_BTN.classList.remove("show");
+					if (
+						window.scrollY >= VISIBLE_TOP_BORDER &&
+						window.scrollY <= VISIBLE_BOTTOM_BORDER
+					) {
+						CLAP_BTN.classList.add("show");
+						COMMENT_BTN.classList.add("show");
+					} else {
+						CLAP_BTN.classList.remove("show");
+						COMMENT_BTN.classList.remove("show");
+					}
 				}
 			}
 		}
-	});
 
-	$(function () {
-		const COMMENT_BTN = document.querySelector(".post-comment-btn img");
-		const COMMENT_AREA = document.querySelector(".post-comment-area");
-		const COMMENT_HIDE_BTN = document.querySelector(".post-comment-hide-btn");
+		{ // コメントエリア表示処理
+			const COMMENT_BTN = document.querySelector(".post-comment-btn img");
+			const COMMENT_AREA = document.querySelector(".post-comment-area");
+			const COMMENT_HIDE_BTN = document.querySelector(".post-comment-hide-btn");
 
-		if (location.hash) {
-			COMMENT_AREA.classList.add("show");
+			if (location.hash) {
+				COMMENT_AREA.classList.add("show");
+			}
+			COMMENT_BTN.addEventListener("click", function () {
+				COMMENT_AREA.classList.toggle("show");
+			});
+			COMMENT_HIDE_BTN.addEventListener("click", function () {
+				COMMENT_AREA.classList.toggle("show");
+			});
 		}
-		COMMENT_BTN.addEventListener("click", function () {
-			COMMENT_AREA.classList.toggle("show");
-		});
-		COMMENT_HIDE_BTN.addEventListener("click", function () {
-			COMMENT_AREA.classList.toggle("show");
-		});
-	});
-
-	// aosライブラリ初期化
-	AOS.init();
+	}
 
 	// swiper.jsライブラリ初期化
 	const mySwiper = new Swiper(".swiper-container", {
@@ -116,14 +126,6 @@
 	// スワイパーコンテンツホバー時に詳細表示
 	$(".swiper-container").hover(function () {
 		$(".swiper-content").toggleClass("show");
-	});
-
-	// ローディングアニメーション非表示
-	$(window).on("load", function () {
-		setTimeout(function () {
-			$("#loading").addClass("loaded");
-			$("#content").addClass("loaded");
-		}, 1200);
 	});
 
 	// Smooth scrolling using jQuery easing
--- header.php
+++ header.php
@@ -25,8 +25,8 @@
 <link href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" rel="stylesheet" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&family=Noto+Serif+JP:wght@400;600&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="//cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css">
-<link rel="stylesheet" href="//unpkg.com/swiper/swiper-bundle.min.css">
+<link rel="stylesheet" href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css">
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
 
 <!-- Core theme CSS (includes Bootstrap)-->
 <link href="<?= get_template_directory_uri(); ?>/style.css" rel="stylesheet" />
